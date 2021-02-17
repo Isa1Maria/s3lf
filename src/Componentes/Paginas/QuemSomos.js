0f3ff6e080c2bf7742cb4b17200c60b1e9bcfabb
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-17 01:46:29 +0000

    Update QuemSomos

--- src/Componentes/Paginas/QuemSomos.js
+++ src/Componentes/Paginas/QuemSomos.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const QuemSomos = () => {
-return <div className="page-heading"><h1>Quem Somos</h1></div>
+return <div className="page-heading">
+    
+    <h1>A S3lf nasce no início de 2019 com o intuito de transformar a percepção que se tem do activídade física orientada, explorando não apenas a sua relevância na saúde física, mas também na saúde mental. 
+São inúmeros os estudos científicos já realizados que atestam a influência que a prática regular de exercício físico pode ter na saúde mental de um indivíduo. Contudo, o nosso trabalho é organizar este processo de modo a atuar nas variáveis que possam estar subaproveitadas e consequentemente criem alguma disrupção na funcionalidade natural de um indivíduo.
+A denominação de S3lf foi escolhida para designar as 3 dimensões que procuramos exaltar e desenvolver: self conscious, ou autoconsciência; self improvement, ou auto-aperfeiçoamento e self suficiente, ou autossuficiência. A proposta é dotar cada indivíduo de um leque de competências que o ajudem a optimizar todos os recursos de que dispões para alcançar a melhor versão possível de si mesmo. 
+Para atingirmos o propósito a que nos propomos, alicerçamos as nossas sessões em pilares específicos:
+- Recurso a estratégias psicoterapêuticas
+- Exercício físico estruturado 
+- Sessões individuais ou para grupos homogéneos nas necessidades apresentadas
+- Acompanhamento constante
+- Avaliações regulares
+- Integração de técnicas de yoga, pilates e meditação.
+Consideramos que nos dias de hoje, o nosso serviço será uma mais valia para qualquer empresa que tenha o interesse genuíno em zelar pelo bem-estar dos seus colaboradores, pois reconhece-se hoje a influência que esta dimensão tem no grau de satisfação no local de trabalho e consequentemente na produtividade. 
+Desenvolvemos um método que definimos como sendo psicofisiológico, onde a dimensão do exercício físico é o estimulo para desencadear as transformações a nível físico e cognitivo. </h1>
+</div>
 }
 
 export default QuemSomos;
