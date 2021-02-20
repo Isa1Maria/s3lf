4a767b353a8af68e427b5224ec0be97c4bf33939
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-20 22:15:32 +0000

    Added styled-components&react-scroll

---
+++ src/Componentes/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                
+            </NavbarContainer>
+        </Nav>
+        </>
+    );
+};
+
+export default Navbar;
