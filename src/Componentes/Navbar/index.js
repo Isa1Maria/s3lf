a8a525f11c8a7b7571c027fb7f35572047d0606e
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-20 23:18:02 +0000

    Added Navbar

--- src/Componentes/Navbar/index.js
+++ src/Componentes/Navbar/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+
 
 const Navbar = () => {
     return (
         <>
         <Nav>
             <NavbarContainer>
-                
+               <NavLogo to="/"> S3LF </NavLogo> 
+               
             </NavbarContainer>
         </Nav>
         </>
