2ab0f2f9a4be059205acf9906e081f9505cc15b6
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-21 00:20:05 +0000

    Added MobileStyle

--- src/Componentes/Navbar/NavbarElements.js
+++ src/Componentes/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import {Link as LinkR} from 'react-router-dom';
+import {Link as LinkS} from 'react-scroll';
 
 export const Nav = styled.nav`
 background: #000;
@@ -28,7 +29,7 @@ max-width: 1100px;
 `
 
 export const NavLogo = styled(LinkR)`
-color: #15315a;
+color: #fff;
 justify-self: flex-start;
 cursor: pointer;
 font-size: 1.5rem;
@@ -37,4 +38,49 @@ align-items: center;
 margin-left: 24px;
 font-weight: bold;
 text-decoration: none;
+`
+
+export const MobileIcon = styled.div`
+display: none;
+
+@media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #fff;
+}
+`
+
+export const NavMenu = styled.ul`
+display: flex;
+align-items: none;
+text-align: center;
+margin-right: -22px;
+
+@media screen and (max-width: 768px) {
+    display: none;
+}
+`
+
+
+export const NavItem = styled.li`
+height: 80px;
+`
+
+export const NavLinks = styled(LinkS)`
+color: #fff;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0 1rem;
+height: 100%;
+cursor: pointer;
+
+&.active {
+    border-bottom: 3px solid #01bf71;
+}
 `
--- src/Componentes/Navbar/index.js
+++ src/Componentes/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import {FaBars} from 'react-icons/fa';
 
 
 const Navbar = () => {
@@ -8,7 +9,23 @@ const Navbar = () => {
         <Nav>
             <NavbarContainer>
                <NavLogo to="/"> S3LF </NavLogo> 
-               
+               <MobileIcon>
+                   <FaBars />
+               </MobileIcon>
+               <NavMenu>
+                   <NavItem>
+                       <NavLinks to="quemsomos">Quem somos</NavLinks>
+                   </NavItem>
+                   <NavItem>
+                       <NavLinks to="serviços">Serviços</NavLinks>
+                   </NavItem>
+                   <NavItem>
+                       <NavLinks to="testemunhos">Testemunhos</NavLinks>
+                   </NavItem>
+                   <NavItem>
+                       <NavLinks to="contactos">Contactos</NavLinks>
+                   </NavItem>
+               </NavMenu>
             </NavbarContainer>
         </Nav>
         </>
