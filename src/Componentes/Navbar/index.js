0e3c8aa644b469c9a17b8b61c44a0327301dfda8
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-21 04:35:00 +0000

    Function SmoothScroll

--- src/Componentes/Navbar/index.js
+++ src/Componentes/Navbar/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
 import {FaBars} from 'react-icons/fa';
+import {IconContext} from 'react-icons/lib';
 
 
 const Navbar = ({toggle}) => {
+
+const [scrollNav, setScrollNav] = useState(false)
+
+const changeNav = () => {
+    if(window.scrollY >=80) {
+        setScrollNav(true)
+    } else {
+        setScrollNav(false)
+    }
+
+}
+
+
+useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+}, [])
+
+
     return (
         <>
-        <Nav>
+        <IconContext.Provider value={{color: '#fff'}}>
+        <Nav scrollNav={scrollNav}>
             <NavbarContainer>
                <NavLogo to="/"> S3LF </NavLogo> 
                <MobileIcon onClick={toggle}>
@@ -28,6 +48,7 @@ const Navbar = ({toggle}) => {
                </NavMenu>
             </NavbarContainer>
         </Nav>
+        </IconContext.Provider>
         </>
     );
 };
